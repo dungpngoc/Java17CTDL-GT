06284f33d48fc5af0c2fa6047dfb22944a45c57f
dungpngoc <117157383+dungpngoc@users.noreply.github.com>
2023-02-03 14:20:07 +0700

    update

---
+++ lesson/src/day02/LC344_ReverseString.java
@@ -0,0 +1,17 @@
+package day02;
+
+import java.util.Arrays;
+
+public class LC344_ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
