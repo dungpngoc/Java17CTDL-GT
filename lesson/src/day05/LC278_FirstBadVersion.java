422ce47810a0e01d7de2c168f1aa8b27abc137b9
dungpngoc <117157383+dungpngoc@users.noreply.github.com>
2023-02-10 23:48:52 +0700

    update

---
+++ lesson/src/day05/LC278_FirstBadVersion.java
@@ -0,0 +1,24 @@
+package day05;
+
+
+public class LC278_FirstBadVersion {
+    public static int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    public static boolean isBadVersion(int version) {
+        if (version >= firstBadVersion(5)) {
+            return true;
+        }
+        return false;
+    }
+}
