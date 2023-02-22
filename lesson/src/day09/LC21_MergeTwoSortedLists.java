0af4f2f6fd2efdde16180ce584e31d4211817fe0
dungpngoc <117157383+dungpngoc@users.noreply.github.com>
2023-02-23 00:24:45 +0700

    update

--- lesson/src/day09/LC21_MergeTwoSortedLists.java
+++ lesson/src/day09/LC21_MergeTwoSortedLists.java
@@ -8,21 +8,14 @@ public class LC21_MergeTwoSortedLists {
         if (l2 == null) {
             return l1;
         }
+
+        // Cái này dùng đệ quy mà a chưa hiểu logic. Hic. A đi ngủ đã mai có time giải thích cho a nhớ . Cám ơn e !!!
         if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
             l2.next = mergeTwoLists(l1, l2.next);
             return l2;
-        }
-    }
-
-    class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int val) {
-            this.val = val;
         }
     }
 }
