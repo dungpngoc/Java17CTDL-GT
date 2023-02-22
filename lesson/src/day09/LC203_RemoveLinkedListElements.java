fa9b2e66cf20fdd64541c580bc08ae176d0bc521
dungpngoc <117157383+dungpngoc@users.noreply.github.com>
2023-02-23 00:15:16 +0700

    update

--- lesson/src/day09/LC203_RemoveLinkedListElements.java
+++ lesson/src/day09/LC203_RemoveLinkedListElements.java
@@ -1,8 +1,30 @@
 package day09;
 
-public class LC203_RemoveLinkedListElements {
-//    public ListNode removeElements(ListNode head, int val) {
-//
-//        return head;
-//    }
+class LC203_RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        // Nếu phần tử đầu = null thì danh sách rỗng, trả về null
+        if(head == null) {
+            return null;
+        }
+
+        // Nếu phần tử tiếp theo rỗng và giá trị đầu = giá trị cần xoá thì trả về null vì danh sách có mỗi 1 phần tử (sau khi xoá sẽ là null)
+        if (head.next == null && head.val == val) {
+            return null;
+        }
+        // Nếu tất cả phần tử đều = val thì sẽ xoá và gán phần tử đầu tiền = phần tử tiếp theo -> xoá đến hết.
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        ListNode curNode = head;
+        while (curNode != null && curNode.next != null) {
+            if (curNode.next.val == val) {
+                curNode.next = curNode.next.next;
+            } else {
+                curNode = curNode.next;
+            }
+        }
+        return head;
+    }
 }
+
